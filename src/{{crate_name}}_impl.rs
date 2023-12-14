9badcd94f838bdd4db00b1e578a36ff1d4d174ed
Uriah <codeitlikemiley@gmail.com>
2023-12-15 01:50:29 +0800

    update pascal case

--- src/{{crate_name}}_impl.rs
+++ src/{{crate_name}}_impl.rs
@@ -1,10 +1,10 @@
 use crate::{{crate_name}}::{hello_service_server::HelloService, HelloRequest, HelloResponse};
 use tonic::{Request, Response, Status};
 
-pub struct {{CrateName}}ServiceImpl {}
+pub struct {{project-name | pascal_case}}ServiceImpl {}
 
 #[tonic::async_trait]
-impl HelloService for {{CrateName}}ServiceImpl {
+impl HelloService for {{project-name | pascal_case}}ServiceImpl {
 async fn hello(&self, request: Request<HelloRequest>) -> Result<Response<HelloResponse>, Status> {
     let name = request.into_inner().name;
     let reply = HelloResponse {
