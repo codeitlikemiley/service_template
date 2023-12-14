59947dcb0cdff2d2fd26c140d876f5ec2e280a25
Uriah <codeitlikemiley@gmail.com>
2023-12-15 05:33:17 +0800

    working service generator with initial test

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,24 @@
 pub mod {{crate_name}};
-mod {{crate_name}}_impl;
+pub mod {{crate_name}}_impl;
+
+#[cfg(test)]
+mod tests {
+    use tonic::Request;
+    use crate::{{crate_name}}::{{crate_name}}_service_server::{{project-name | pascal_case}}Service;
+    use crate::{{crate_name}}::{{project-name | pascal_case}}Request;
+    use crate::{{crate_name}}_impl::{{project-name | pascal_case}}ServiceImpl;
+
+    #[tokio::test]
+    async fn test_{{crate_name}}() {
+        let service  = {{project-name | pascal_case}}ServiceImpl {};
+        let request = Request::new({{project-name | pascal_case}}Request {
+            name: "Tonic".into(),
+        });
+        let response =  service.make_{{crate_name}}(request).await.unwrap();
+        assert_eq!(
+            response.into_inner().message,
+            "Hello Tonic!".to_string()
+        );
+    }
+}
+
--- src/{{crate_name}}_impl.rs
+++ src/{{crate_name}}_impl.rs
@@ -1,13 +1,14 @@
-use crate::{{crate_name}}::{{{crate_name}}_service_server::HelloService, HelloRequest, HelloResponse};
+use crate::{{crate_name}}::{{"{"}}{{crate_name}}_service_server::{{project-name | pascal_case}}Service, {{project-name | pascal_case}}Request, {{project-name | pascal_case}}Response};
 use tonic::{Request, Response, Status};
 
 pub struct {{project-name | pascal_case}}ServiceImpl {}
 
 #[tonic::async_trait]
 impl {{project-name | pascal_case}}Service for {{project-name | pascal_case}}ServiceImpl {
-async fn hello(&self, request: Request<HelloRequest>) -> Result<Response<HelloResponse>, Status> {
+
+async fn make_{{crate_name}}(&self, request: Request<{{project-name | pascal_case}}Request>) -> Result<Response<{{project-name | pascal_case}}Response>, Status> {
     let name = request.into_inner().name;
-    let reply = HelloResponse {
+    let reply = {{project-name | pascal_case}}Response {
         message: format!("Hello {}!", name),
     };
     Ok(Response::new(reply))
