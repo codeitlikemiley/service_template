17b808668e4dcc39b27bc329e334584c42e4562a
Uriah <codeitlikemiley@gmail.com>
2023-12-15 01:56:11 +0800

    fix generated name

--- src/{{crate_name}}_impl.rs
+++ src/{{crate_name}}_impl.rs
@@ -1,10 +1,10 @@
-use crate::{{crate_name}}::{hello_service_server::HelloService, HelloRequest, HelloResponse};
+use crate::{{crate_name}}::{{{crate_name}}_service_server::HelloService, HelloRequest, HelloResponse};
 use tonic::{Request, Response, Status};
 
 pub struct {{project-name | pascal_case}}ServiceImpl {}
 
 #[tonic::async_trait]
-impl HelloService for {{project-name | pascal_case}}ServiceImpl {
+impl {{project-name | pascal_case}}Service for {{project-name | pascal_case}}ServiceImpl {
 async fn hello(&self, request: Request<HelloRequest>) -> Result<Response<HelloResponse>, Status> {
     let name = request.into_inner().name;
     let reply = HelloResponse {
