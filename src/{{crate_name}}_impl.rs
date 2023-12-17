4a92bd7a84e46da2b9b7a3fd0eca7f95781d99ba
Uriah <codeitlikemiley@gmail.com>
2023-12-17 08:05:58 +0800

    added new and attach_reflection method

--- src/{{crate_name}}_impl.rs
+++ src/{{crate_name}}_impl.rs
@@ -1,17 +1,33 @@
-use crate::{{crate_name}}::{{"{"}}{{crate_name}}_service_server::{{project-name | pascal_case}}Service, {{project-name | pascal_case}}Request, {{project-name | pascal_case}}Response};
+use crate::{{service_name}}::{{service_name}}_service_server::{{service_name | pascal_case}}ServiceServer;
+use crate::{{service_name}}::{{"{"}}{{service_name}}_service_server::{{service_name | pascal_case}}Service, {{service_name | pascal_case}}Request, {{service_name | pascal_case}}Response};
+use std::error::Error;
 use tonic::{Request, Response, Status};
+use tonic_reflection::server::{Builder, ServerReflection, ServerReflectionServer};
+
+const FILE_DESCRIPTOR_SET: &[u8] = include_bytes!("{{service_name}}_reflection_descriptor.bin");
 
 #[derive(Default)]
-pub struct {{project-name | pascal_case}}ServiceImpl {}
+pub struct {{service_name | pascal_case}}ServiceImpl {}
+
+impl {{service_name | pascal_case}}ServiceImpl {
+    pub fn new() -> {{service_name | pascal_case}}ServiceServer<{{service_name | pascal_case}}ServiceImpl> {
+        {{service_name | pascal_case}}ServiceServer::new({{service_name | pascal_case}}ServiceImpl {})
+    }
+    pub fn attach_reflection() -> Result<ServerReflectionServer<impl ServerReflection + 'static>, Box<dyn Error>> {
+        Builder::configure()
+            .register_encoded_file_descriptor_set(FILE_DESCRIPTOR_SET)
+            .build()
+            .map_err(|e| Box::new(e) as Box<dyn Error>)
+    }
+}
 
 #[tonic::async_trait]
-impl {{project-name | pascal_case}}Service for {{project-name | pascal_case}}ServiceImpl {
-
-async fn {{rpc_method}} (&self, request: Request<{{project-name | pascal_case}}Request>) -> Result<Response<{{project-name | pascal_case}}Response>, Status> {
-    let name = request.into_inner().name;
-    let reply = {{project-name | pascal_case}}Response {
-        message: format!("Hello {}!", name),
-    };
-    Ok(Response::new(reply))
-}
+impl {{service_name | pascal_case}}Service for {{service_name | pascal_case}}ServiceImpl {
+    async fn {{rpc_method}}(&self, request: Request<{{service_name | pascal_case}}Request>) -> Result<Response<{{service_name | pascal_case}}Response>, Status> {
+        let name = request.into_inner().name;
+        let reply = {{service_name | pascal_case}}Response {
+            message: format!("Hello {}!", name),
+        };
+        Ok(Response::new(reply))
+    }
 }
